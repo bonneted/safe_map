775379572782820117d242b6620c968bfb2d7c9c
Damien Bonnet-Eymard <damien.bonnet-eymard@kuleuven.be>
2024-02-07 22:39:20 +0100

    add info popup

--- static/js/map.js
+++ static/js/map.js
@@ -212,3 +212,33 @@ function getColorForAlpha(alpha) {
     return `rgba(${r},${g},0,0.9)`;
 }
 
+// Get the modal
+var modal = document.getElementById("helpModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("helpBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the page loads, open the modal
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
